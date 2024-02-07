e8719b7002e8db40f99bb1f9af4e5a820bedb2fb
bluenote28 <136389327+bluenote28@users.noreply.github.com>
2024-02-06 20:40:11 -0600

    Update gamemanager.py

--- gamemanager.py
+++ gamemanager.py
@@ -1,5 +1,4 @@
 import tkinter
-import tkinter.messagebox
 
 class GameManager:
 
@@ -8,49 +7,19 @@ class GameManager:
         self.window = window
         self.end_screen = tkinter.Label(self.window, text="You have reached the end. Congrats!!")
         self.level_count = len(levels)
-        self.next_level_button = tkinter.Button(self.window, text="Next Level", command=self.destroy_level)
-        self.current_level = 0
-
-    def add_level(self, level):
-        self.levels.append(level)
+        self.current_question_index = 0
 
     def start_game(self):
-        self.levels[self.current_level].display_question()
-        self.next_level_button.grid(column=3, row=len(self.levels[self.current_level].answers) + 1)
 
-    def start_next_level(self):
-        if self.current_level >= self.level_count:
-            self.next_level_button.destroy()
-            self.end_screen.pack()
-        else:
-            self.levels[self.current_level].display_question()
-            self.next_level_button.grid(column=3, row=len(self.levels[self.current_level].answers) + 1)
+        current_question = self.levels[self.current_question_index]
+        current_question.display_question()
+        print(current_question.question_displayed)
+        current_question.destroy_question()
+        print(current_question.question_displayed)
 
-    def destroy_level(self):
+        #while self.current_question_index < self.level_count:
+        #while current_question.question_displayed:
+        #    print(self.current_question_index)
 
-        message = tkinter.messagebox
-        
-        if self.levels[self.current_level].question_answered:
-
-            count = 0
-
-            while count < len(self.levels[self.current_level].answers):
-                self.levels[self.current_level].answer_check_labels[count].destroy()
-                count += 1
-            self.levels[self.current_level].answer_check_labels.clear()
-
-            for label in self.levels[self.current_level].labels:
-                label.destroy()
-
-            for entry in self.levels[self.current_level].entries:
-                entry.destroy()
-
-            self.levels[self.current_level].answer_check_button.destroy()
-            self.levels[self.current_level].win_label.destroy()
-            self.levels[self.current_level].level_title_label.destroy()
-            self.current_level += 1
-            self.start_next_level()
-        else:
-            message.showerror(title="Warning", message="You have not answered all questions correctly.", parent=self.window)
-        
-
+        #maybe put all this in main#######
+            
