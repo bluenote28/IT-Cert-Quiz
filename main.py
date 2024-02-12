b43f1bc57b52d109167a34f01cb8077f4b81301d
bluenote28 <136389327+bluenote28@users.noreply.github.com>
2024-02-12 15:06:17 -0600

    fixes issue of changing questions freezing

--- main.py
+++ main.py
@@ -1,30 +1,43 @@
 import tkinter
-from gamemanager import GameManager
 from question import OrderedFillInTheBlank, MultipleChoice
 
-guess_game_window = tkinter.Tk()
-guess_game_window.title("Name IT terms")
-guess_game_window.minsize(width=500, height=300)
+quiz_window = tkinter.Tk()
+quiz_window.title("Name IT terms")
+quiz_window.minsize(width=500, height=300)
+current_question_index = 0
 
-def start_game(game):
-    welcome_button.destroy()
-    welcome_label.destroy()
-    game.start_game()
-
-osi_question = OrderedFillInTheBlank(guess_game_window, "Name all of the layers of the OSI Model", ["Physical", "Data Link", "Network", "Transportation", "Session", "Presentation", "Application"], "Layer", "the OSI model")
-##encapsulation = OrderedFillInTheBlank(guess_game_window, "Name the PDU at each level", ["Bit", "Frame", "Packet", "Segment", "Data", "Data", "Data"], "the OSI Model", "Layer")
+osi_question = OrderedFillInTheBlank(quiz_window, "Name all of the layers of the OSI Model", ["Physical", "Data Link", "Network", "Transportation", "Session", "Presentation", "Application"], "Layer", "the OSI model")
+encapsulation = OrderedFillInTheBlank(quiz_window, "Name the PDU at each level", ["Bit", "Frame", "Packet", "Segment", "Data", "Data", "Data"], "the OSI Model", "Layer")
 ##tcp_udp = GenerateLevel(2, "The two main layer four protocols from a to z", "2", "Layer Four protocol", ["TCP", "UDP"], guess_game_window)
 ##man_question = MultipleChoice(guess_game_window, "Which network type covers a city wide area?", ['LAN', 'WAN', 'PAN', 'MAN'], 3)
 
-game = GameManager([osi_question], guess_game_window)
+questions = [osi_question, encapsulation]
 
-welcome_label = tkinter.Label(guess_game_window, text="Welcome to the IT terms quiz")
+welcome_label = tkinter.Label(quiz_window, text="Welcome to the IT terms quiz")
 welcome_label.pack()
 
-welcome_button = tkinter.Button(guess_game_window, text="Start Quiz", command=lambda: start_game(game))
+def check_question():
+    global current_question_index
+
+    if not questions[current_question_index].question_displayed and current_question_index < len(questions):
+        current_question_index += 1
+        questions[current_question_index].display_question()
+
+    quiz_window.after(1, check_question)
+
+def display_question():
+
+    welcome_button.destroy()
+    welcome_label.destroy()
+    
+    questions[current_question_index].display_question()
+    quiz_window.after(1, check_question)
+
+
+welcome_button = tkinter.Button(quiz_window, text="Start Quiz", command=display_question)
 welcome_button.pack()
 
-guess_game_window.mainloop()
+quiz_window.mainloop()
 
 
 
